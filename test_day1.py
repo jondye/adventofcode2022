e49ef975a458d54953441da6e4e107847b2984be
Jon Dye <jon@pecorous.co.uk>
2022-12-01 10:38:27 +0000

    Apply flake8 linting

--- test_day1.py
+++ test_day1.py
@@ -5,8 +5,7 @@
 https://adventofcode.com/2022/day/1
 """
 
-from day1 import *
-import pytest
+from day1 import day1_1, sum_calories
 from hamcrest import assert_that, contains_exactly
 
 calorie_list = """1000
@@ -25,8 +24,10 @@ calorie_list = """1000
 10000
 """
 
+
 def test_day1():
     assert day1_1(calorie_list.splitlines(True)) == 24000
+
 
 def test_sum_calories():
     assert_that(
