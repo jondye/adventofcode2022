10576b9ed9fc961ff43d68896b91ac8ee07c6c55
Jon Dye <jon@pecorous.co.uk>
2022-12-05 14:51:20 +0000

    Solve day3.2

--- day3.py
+++ day3.py
@@ -2,21 +2,41 @@
 # -*- coding: utf-8 -*-
 
 from string import ascii_letters
+from itertools import tee
 
 
-def day3(contents):
-    total = 0
-    for rucksack in contents:
-        first_half = set(rucksack[:len(rucksack)//2])
-        second_half = set(rucksack[len(rucksack)//2:])
-        common = first_half.intersection(second_half).pop()
-        total += ascii_letters.index(common) + 1
-    return total
+def day3_1(contents):
+    return sum(
+        priority(common(rucksack.strip()))
+        for rucksack in contents)
+
+
+def common(rucksack):
+    first_half = set(rucksack[:len(rucksack)//2])
+    second_half = set(rucksack[len(rucksack)//2:])
+    return set.intersection(first_half, second_half).pop()
+
+
+def priority(letter):
+    return ascii_letters.index(letter) + 1
+
+
+def day3_2(contents):
+    rucksacks = iter(set(line.strip()) for line in contents)
+    return sum(
+        priority(badge(group))
+        for group in zip(rucksacks, rucksacks, rucksacks))
+
+
+def badge(group):
+    return set.intersection(*group).pop()
 
 
 def main():
     with open('input3.txt') as contents:
-        print(f"Total of duplicate item priorities is {day3(contents)}")
+        part1, part2 = tee(contents)
+        print(f"Total of duplicate item priorities is {day3_1(part1)}")
+        print(f"Total of group badge priorities is {day3_2(part2)}")
 
 
 if __name__ == '__main__':
--- test_day3.py
+++ test_day3.py
@@ -5,7 +5,7 @@
 https://adventofcode.com/2022/day/3
 """
 
-from day3 import day3
+from day3 import day3_1, day3_2
 
 from io import StringIO
 
@@ -21,5 +21,9 @@ CrZsJsPPZsGzwwsLwLmpwMDw
 """
 
 
-def test_day3():
-    assert_that(day3(StringIO(contents)), equal_to(157))
+def test_day3_1():
+    assert_that(day3_1(StringIO(contents)), equal_to(157))
+
+
+def test_day3_2():
+    assert_that(day3_2(StringIO(contents)), equal_to(70))
